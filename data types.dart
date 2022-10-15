232f69576738e7070795b0277e73f2c29cdaa2f7
NimraAbid05 <nimram214@gmail.com>
2022-10-15 08:53:05 -0700

    Data types

---
+++ data types.dart
@@ -0,0 +1,32 @@
+void main(){
+  //data types
+  //1) String 
+  String name="Hadi";
+  String email="hadi123@gmail.com";
+  print("String");
+  print(name);
+  print(email);
+  print("***************");
+
+  //2) Numbers
+  int age=17;
+  double weight=35.5;
+  num height=5.5;
+  num agE=17;  //num accept float or whole both numbers
+  print("Numbers");
+  print(age);
+  print(weight);
+  print(height);
+  print(agE);
+  print("***************");
+
+
+  //3) boolean   check conditions & return true or false
+  int num1=23;
+  int num2=45;
+  print("Boolean");
+  print("num1 > num2");
+  print(num1>num2);
+  print("num1 < num2");
+  print(num1<num2);
+}
